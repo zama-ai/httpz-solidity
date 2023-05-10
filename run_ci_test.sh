0a1758654719fd6828e570754fc903931c41d4cf
Levent Demir <levent.demir@zama.ai>
2023-05-10 14:40:33 +0200

    feature(ci): fix script name

--- run_ci_test.sh
+++ run_ci_test.sh
@@ -8,4 +8,4 @@ fi
 key=$1
 PRIVATE_KEY=$(docker compose -f ../zbc-development/docker-compose.validator.yml exec validator evmosd --home /root/.evmosd keys unsafe-export-eth-key $key --keyring-backend test)
 echo "Get address from private key: $PRIVATE_KEY"
-docker compose -f docker-compose.yml run app python demo_test_high_level.py $PRIVATE_KEY
+docker compose -f docker-compose.yml run app python demo_test_high_level_fhe_tool.py $PRIVATE_KEY
