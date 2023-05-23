6245a19c4537e42e284af602045a1f1e33b814d3
Levent Demir <levent.demir@zama.ai>
2023-05-23 11:54:50 +0200

    feature(local): add script to use locally generated fhe keys for test

---
+++ prepare_fhe_keys_from_fhe_tool.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+if [ "$#" -ne 1 ]; then
+    echo "Please give the path where all the keys are stored"
+    exit
+fi
+
+NETWORK_KEYS_PUBLIC_PATH=./volumes/network-public-fhe-keys
+NETWORK_KEYS_PRIVATE_PATH=./volumes/network-private-fhe-keys
+USERS_KEYS_PATH=./volumes/users-fhe-keys
+KEYS_FULL_PATH=$1
+
+MANDATORY_KEYS_LIST=('global_uncompressed_pks.bin' 'alice_cks.bin' 'bob_cks.bin' 'carol_cks.bin')
+ 
+echo "check folder $KEYS_FULL_PATH"
+for key in "${MANDATORY_KEYS_LIST[@]}"
+    do
+        if [ ! -f "$KEYS_FULL_PATH/$key" ]; then
+            echo "#####ATTENTION######"
+            echo "$key does not exist!"
+            echo "####################"
+            exit
+        else
+            echo "$key exists, nice!"
+        fi
+done
+
+mkdir -p $NETWORK_KEYS_PUBLIC_PATH
+mkdir -p $USERS_KEYS_PATH
+
+key="global_uncompressed_pks.bin"
+echo "Copying $key to $NETWORK_KEYS_PUBLIC_PATH, please wait ..."
+cp $KEYS_FULL_PATH/$key $NETWORK_KEYS_PUBLIC_PATH/pks
+
+key="alice_cks.bin"
+echo "Copying $key to $USERS_KEYS_PATH, please wait ..."
+cp $KEYS_FULL_PATH/$key $USERS_KEYS_PATH/alice_cks.bin
+
+key="bob_cks.bin"
+echo "Copying $key to $USERS_KEYS_PATH, please wait ..."
+cp $KEYS_FULL_PATH/$key $USERS_KEYS_PATH/bob_cks.bin
+
+key="carol_cks.bin"
+echo "Copying $key to $USERS_KEYS_PATH, please wait ..."
+cp $KEYS_FULL_PATH/$key $USERS_KEYS_PATH/carol_cks.bin
+
