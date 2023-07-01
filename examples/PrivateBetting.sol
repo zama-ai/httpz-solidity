e0ae0471df0c77b65bf6ef8d65b38e3756b723a4
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2023-07-01 16:19:00 +0200

    Update PrivateBetting.sol

--- examples/PrivateBetting.sol
+++ examples/PrivateBetting.sol
@@ -2,9 +2,9 @@
 
 pragma solidity >=0.8.13 <0.9.0;
 
-import "../lib/TFHE.sol";
+import "./lib/TFHE.sol";
 
-contract PrivateBetting {
+contract PrivateBet {
     // The owner of the contract.
     address internal contractOwner;
 
@@ -13,22 +13,25 @@ contract PrivateBetting {
     enum BetState {PENDING, WON, LOST}
     
     struct Bet {
-        address user;
         uint256 gameId;
         euint32 amount;
-        uint256 betOption; // User bets on this outcome option (0 => team A wins, 1 => team B wins, etc.)
+        uint256 option; // User bets on this outcome option (0 => team A wins, 1 => team B wins, etc.)
         BetState state;
     }
     
     struct Game {
         string description;
         bool isOpen;
-        bool winningsDistributed;
         uint256 numBets;
         uint256 maxBetOptions;
+        bool[] results;
     }
 
-    Bet[] bets;
+    // gameId => user => bet
+    mapping(uint256 => mapping(address => Bet)) bets;
+
+    // gameId => results
+    mapping(uint256 => bool[]) results;
 
     Game[] public games;
     uint256 public numGames;
@@ -36,7 +39,7 @@ contract PrivateBetting {
     
     event NewGame(string description);
     event NewBet(uint256 gameID, uint256 betOption, euint32 encryptedBetAmount);
-    event GameClosed(uint256 gameID, string description);
+    event GameClosed(uint256 gameID, string description, bool[] results);
 
     constructor(address _encryptedToken) {
         numGames = 0;
@@ -49,7 +52,7 @@ contract PrivateBetting {
     }
     
     function createGame(string memory _description, uint256 _maxBetOptions) public onlyContractOwner {
-        games.push(Game(_description, true, false, 0, _maxBetOptions));
+        games.push(Game(_description, true, 0, _maxBetOptions, new bool[](0)));
         numGames++;
         emit NewGame(_description);
     }
@@ -64,48 +67,39 @@ contract PrivateBetting {
         euint32 encryptedBetAmount = TFHE.asEuint32(_encryptedAmount);
         encryptedToken.transferFrom(msg.sender, address(this), encryptedBetAmount);
 
-        bets.push(
-            Bet(
-                msg.sender,
+        bets[_gameId][msg.sender] = Bet(
                 _gameId,
                 encryptedBetAmount,
                 _betOption,
                 BetState.PENDING
-            )
-        );
+            );
 
         emit NewBet(_gameId, _betOption, encryptedBetAmount);
     }
     
-    function closeGame(uint256 _gameId) public onlyContractOwner {
+    function closeGame(uint256 _gameId, bool[] calldata _betResults) public onlyContractOwner {
         require(_gameId < numGames, "Invalid game ID");
         Game storage game = games[_gameId];
         require(game.isOpen, "Game is already closed");
+        require(_betResults.length == game.maxBetOptions, "Results for every option must be provided");
+
         game.isOpen = false;
-        emit GameClosed(_gameId, games[_gameId].description);
+        game.results = _betResults;
+
+
+        emit GameClosed(_gameId, games[_gameId].description, _betResults);
     }
 
-    function distributeWinnings(uint256 _gameId, bool[] calldata _betResults) public onlyContractOwner {
+    function getWinnings(uint256 _gameId) public {
         require(_gameId < numGames, "Invalid game ID");
         Game storage game = games[_gameId];
         require(!game.isOpen, "Game is not yet closed");
-        require(_betResults.length == game.maxBetOptions, "Results for every option must be provided");
-        require(!game.winningsDistributed, "Winnings already distributed for this game");
 
-
-        for (uint i = 0; i < bets.length; i++) {
-            Bet storage bet = bets[i];
-
-            if (bet.gameId == _gameId) { // fait à la va-vite :^)
-                if (_betResults[bet.betOption]) { 
-                    bet.state = BetState.WON;
-                    encryptedToken.transfer(bet.user, TFHE.mul(BET_MULTIPLIER, bet.amount));
-                } else {
-                    bet.state = BetState.LOST;
-                }
-            }
+        Bet storage bet = bets[_gameId][msg.sender];
+        if (game.results[bet.option]) {
+            bet.state = BetState.WON;
+            encryptedToken.transfer(msg.sender, TFHE.mul(BET_MULTIPLIER, bet.amount));
         }
-        game.winningsDistributed = true;
     }
 }
 
