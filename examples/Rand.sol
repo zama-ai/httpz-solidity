5620ae28cd952a9e973e1a70a6354410a15c6494
Joseph-André Turk <josephandre.turk@zama.ai>
2024-10-10 16:07:42 +0200

    feat: updated TFHEExecutor to support new types
    chore: eq-neq  bool support and bitwise scalar support
    
    chore: overloads for fheEq and fheNe
    
    fix: reverts onchain when rhs is 0 in fheDiv-fheRem
    
    feat: added support for scalar and,or,xor
    
    chore: cleanup codegen
    
    feat: adds euint128 and euint256 types
    
    chore: removes ebool fheRandBounded and adds manual tests
    
    feat: adds new types for fheRand
    
    feat: adds trivialEncrypt for ebytesXXX
    
    feat: adds fheEq fheNe ifThenElse for new types
    
    chore: rename asyncDecrypt() to initGateway()

--- examples/Rand.sol
+++ examples/Rand.sol
@@ -6,19 +6,39 @@ import "../lib/TFHE.sol";
 
 /// @notice Contract for generating random encrypted numbers
 contract Rand {
-    /// @notice Encrypted unsigned integers of various sizes
+    ebool public valueb;
+    euint4 public value4;
     euint8 public value8;
     euint16 public value16;
     euint32 public value32;
     euint64 public value64;
     euint64 public value64Bounded;
+    euint128 public value128;
+    euint256 public value256;
+    ebytes64 public value512;
+    ebytes128 public value1024;
+    ebytes256 public value2048;
 
     /// @notice Constructor to set FHE configuration
     constructor() {
         TFHE.setFHEVM(FHEVMConfig.defaultConfig());
     }
 
-    /// @notice Generate random 8-bit encrypted unsigned integer
+    function generateBool() public {
+        valueb = TFHE.randEbool();
+        TFHE.allowThis(valueb);
+    }
+
+    function generate4() public {
+        value4 = TFHE.randEuint4();
+        TFHE.allowThis(value4);
+    }
+
+    function generate4UpperBound(uint8 upperBound) public {
+        value4 = TFHE.randEuint4(upperBound);
+        TFHE.allowThis(value4);
+    }
+
     function generate8() public {
         value8 = TFHE.randEuint8();
         TFHE.allowThis(value8);
@@ -63,9 +83,7 @@ contract Rand {
         TFHE.allowThis(value64);
     }
 
-    /// @notice Generate random 64-bit encrypted unsigned integer with upper bound
-    /// @param upperBound The maximum value (exclusive) for the generated number
-    function generate64UpperBound(uint32 upperBound) public {
+    function generate64UpperBound(uint64 upperBound) public {
         value64 = TFHE.randEuint64(upperBound);
         TFHE.allowThis(value64);
     }
@@ -83,5 +101,40 @@ contract Rand {
         value64 = TFHE.randEuint64();
         TFHE.allowThis(value64);
         revert();
+    }
+
+    function generate128() public {
+        value128 = TFHE.randEuint128();
+        TFHE.allowThis(value128);
+    }
+
+    function generate128UpperBound(uint128 upperBound) public {
+        value128 = TFHE.randEuint128(upperBound);
+        TFHE.allowThis(value128);
+    }
+
+    function generate256() public {
+        value256 = TFHE.randEuint256();
+        TFHE.allowThis(value256);
+    }
+
+    function generate256UpperBound(uint256 upperBound) public {
+        value256 = TFHE.randEuint256(upperBound);
+        TFHE.allowThis(value256);
+    }
+
+    function generate512() public {
+        value512 = TFHE.randEbytes64();
+        TFHE.allowThis(value512);
+    }
+
+    function generate1024() public {
+        value1024 = TFHE.randEbytes128();
+        TFHE.allowThis(value1024);
+    }
+
+    function generate2048() public {
+        value2048 = TFHE.randEbytes256();
+        TFHE.allowThis(value2048);
     }
 }
